6c6cb3a278c78d1f1e40bf10dfa3684f2f3af81d
olliepegram <olliepegram@gmail.com>
2024-02-06 11:59:01 +1100

    porject section design changes and fixes

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,4 +1,6 @@
 import styles from './Project.module.css';
+import macbook from './assets/macbook.png';
+import runrampup from './assets/runrampup.png';
 
 const latestWorkData = [
 	{
@@ -9,39 +11,59 @@ const latestWorkData = [
 		github: 'https://github.com/olliepegram/RunRampUp',
 		bgColor: '#E6F1FC',
 		tags: ['React', 'HTML', 'CSS'],
+		img: runrampup,
+		second: false,
+		height: '250px',
 	},
 	{
 		title: 'Portfolio',
-		body: `The site you're on!`,
+		body: `The site you're on! Spent more time designing this than I did coding it and was a lot of fun to create.`,
 		github: 'https://github.com/olliepegram/Portfolio-2024',
 		bgColor: '#FDF1F1',
 		tags: ['React', 'HTML', 'CSS'],
+		img: macbook,
+		second: true,
+		height: '250px',
 	},
 ];
 
 function Project() {
 	return (
-		<div>
+		<>
 			{latestWorkData.map((work, index) => (
 				<div
 					style={{ backgroundColor: work.bgColor }}
 					key={index}
 					className={styles.projectSection}
 				>
-					<div className={styles.projectWrapper}>
+					<div
+						className={`${styles.projectWrapper} ${
+							work.second ? styles.second : ''
+						}`}
+					>
 						<div className={styles.projectText}>
 							<h2>{work.title}</h2>
 							{work.tags.map((tag) => (
-								<div key={index}>{tag}</div>
+								<div
+									className={styles.tags}
+									key={index}
+								>
+									<span>{tag}</span>
+								</div>
 							))}
 							<p>{work.body}</p>
-							<button>See this project</button>
+							<button>Read More</button>
 						</div>
-						<div className={styles.projectImg}></div>
+						<div className={styles.projectImg}>
+							<img
+								style={{ height: work.height }}
+								src={work.img}
+							/>
+						</div>
 					</div>
 				</div>
 			))}
-		</div>
+		</>
 	);
 }
 
